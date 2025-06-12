5ea545826ed29ba0e7df67076e1290e81fdb306c
Tamara060890 <toma_tomova@hotmail.com>
2025-06-12 11:36:06 +0200

    Update IceCreamSeller.java

--- Icecream_Shop/src/be/intecbrussel/sellers/IceCreamSeller.java
+++ Icecream_Shop/src/be/intecbrussel/sellers/IceCreamSeller.java
@@ -3,9 +3,9 @@ package be.intecbrussel.sellers;
 import be.intecbrussel.eatables.*;
 
 public interface IceCreamSeller extends Profitable{
-    public Cone orderCone(Flavor[] balls);
-    public IceRocket orderIceRocket();
-    public Magnum orderMagnum(MagnumType type);
+    Cone orderCone(Flavor[] balls);
+    IceRocket orderIceRocket();
+    Magnum orderMagnum(MagnumType type);
 
     @Override
     double getProfit();
